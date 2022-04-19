7177d8049b6a3793d38a5b74b95ccbf9bdea2cb7
Abid Hasan <abidhasan@gmail.com>
2022-04-18 21:51:24 -0700

    Add search index class

---
+++ Search/Search/SearchCore/CorpusSearch.cs
@@ -0,0 +1,63 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+namespace Search.SearchCore
+{
+    public delegate double GetStringDistance(string stringA, string stringB);
+
+    public class CorpusSearch
+    {
+        private readonly GetStringDistance StringDistanceComparator;
+        private readonly int TokenizationGranularity;
+        private readonly SearchIndex SearchIndex;
+
+        public CorpusSearch(GetStringDistance comparator, int tokenizationGranularity, SearchIndex searchIndex)
+        {
+            StringDistanceComparator = comparator;
+            TokenizationGranularity = tokenizationGranularity;
+            SearchIndex = searchIndex;
+        }
+
+        public Dictionary<Verse, double> DoSearch(string searchTerm, double threshold=0)
+        {
+            Dictionary<Verse, double> searchResults = new();
+            string searchWords = SearchHelpers.RemoveDiacriticsFromText(searchTerm.ToLower().Replace(" ", ""));
+
+            IEnumerable<string> tokens = SearchHelpers.GenerateTokens(searchWords, TokenizationGranularity);
+
+            foreach (string token in tokens)
+            {
+                // Look up in search index and score candidates
+                IEnumerable<SearchIndexMatch> candidates = SearchIndex.GetCandidateVersesForToken(token);
+
+                foreach (SearchIndexMatch candidate in candidates)
+                {
+                    // Compute lexical similarity
+                    double currentCandidateScore = ComputeLexicalSimilarity(searchWords, candidate);
+                    if (currentCandidateScore < threshold) continue;
+
+                    searchResults.TryGetValue(candidate.Verse, out double previousCandidateScore);
+                    searchResults[candidate.Verse] = Math.Max(previousCandidateScore, currentCandidateScore);
+                }
+            }
+
+            return searchResults;
+        }
+
+        private double ComputeLexicalSimilarity(string searchTerm, SearchIndexMatch candidate)
+        {
+            double lexicalSimilarity = 0;
+
+            for (int i = searchTerm.Length;  i < searchTerm.Length + 4; i++)
+            {
+                string candidateVerseText = candidate.GetNCharactersFromMatchPosition(i);
+                double currentSimilarity = StringDistanceComparator(searchTerm, candidateVerseText);
+                lexicalSimilarity = Math.Max(lexicalSimilarity, currentSimilarity);
+            }
+
+            return lexicalSimilarity;
+        }
+    }
+}
---
+++ Search/Search/SearchCore/SearchIndex.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Search.SearchCore
+{
+    public class SearchIndex
+    {
+        public readonly Dictionary<string, HashSet<SearchIndexMatch>> searchIndex = new();
+
+        public SearchIndex(List<Verse> verses, Func<string, IEnumerable<string>> tokenGenerator)
+        {
+            foreach (Verse verse in verses)
+            {
+                string wordsInCurrentVerse = verse.SpacelessNormalizedText;
+                var tokens = tokenGenerator(wordsInCurrentVerse).ToArray();
+
+                for (int i = 0; i < tokens.Length; i++)
+                {
+                    searchIndex.TryAdd(tokens[i], new HashSet<SearchIndexMatch>());
+                    searchIndex[tokens[i]].Add(new SearchIndexMatch(verse, i));
+                }
+            }
+        }
+
+        public IEnumerable<SearchIndexMatch> GetCandidateVersesForToken(string token)
+        {
+            if (searchIndex.ContainsKey(token))
+            {
+                return searchIndex[token].ToList();
+            }
+
+            return new List<SearchIndexMatch>();
+        }
+    }
+}
