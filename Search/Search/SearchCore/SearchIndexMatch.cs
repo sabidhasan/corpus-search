872dfe3e3d5c92c6619240404bd8b082ef453ba7
Abid Hasan <abidhasan@gmail.com>
2022-04-18 21:50:26 -0700

    Add auxiliary classes

---
+++ Search/Search/SearchCore/SearchIndexMatch.cs
@@ -0,0 +1,43 @@
+﻿#nullable enable
+
+using System;
+
+namespace Search.SearchCore
+{
+    public class SearchIndexMatch
+    {
+        public int MatchIndex;
+        public Verse Verse;
+
+        public SearchIndexMatch(Verse verse, int matchIndex)
+        {
+            MatchIndex = matchIndex;
+            Verse = verse;
+        }
+
+        public override int GetHashCode()
+        {
+            string str = $"{Verse.Reference.Chapter}-{Verse.Reference.Verse}";
+            return string.GetHashCode(str);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+
+            SearchIndexMatch match = (SearchIndexMatch)obj;
+            return match.Verse.Reference.Chapter == Verse.Reference.Chapter &&
+                match.Verse.Reference.Verse == Verse.Reference.Verse;
+        }
+
+        public string GetNCharactersFromMatchPosition(int characterCount)
+        {
+            int verseLengthFromIndex = Verse.SpacelessNormalizedText.Length - MatchIndex;
+            int maxPossibleCharacters = Math.Min(characterCount, verseLengthFromIndex);
+            return Verse.SpacelessNormalizedText.Substring(MatchIndex, maxPossibleCharacters);
+        }
+    }
+}
---
+++ Search/Search/SearchCore/Verse.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+
+namespace Search.SearchCore
+{
+    public class Verse
+    {
+        public (int Chapter, int Verse) Reference;
+        public string Text;
+        public string SpacelessNormalizedText;
+
+        public Verse(int chapter, int verseNumber, string text)
+        {
+            Reference = (chapter, verseNumber);
+            Text = text;
+            SpacelessNormalizedText = SearchHelpers
+                .RemoveDiacriticsFromText(text.ToLower())
+                .Replace(" ", "");
+        }
+
+        public override string ToString()
+        {
+            return $"{Reference.Chapter}:{Reference.Verse}";
+        }
+    }
+}
