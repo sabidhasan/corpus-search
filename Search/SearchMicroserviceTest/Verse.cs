8df75e6fce0cec35295658397aa1cb8dabb51cda
Abid Hasan <abidhasan@gmail.com>
2022-04-19 20:54:24 -0700

    Add tests

---
+++ Search/SearchMicroserviceTest/Verse.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Search.SearchCore;
+
+namespace SearchMicroserviceTest
+{
+    [TestFixture]
+    public class VerseTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void StringifiesTheVerse()
+        {
+            var verse = new Verse(1, 2, "");
+            Assert.AreEqual(verse.ToString(), "1:2");
+        }
+
+        [Test]
+        public void RemovesDiacritics()
+        {
+            var verse = new Verse(1, 2, "some `test ža 'here al-test");
+            Assert.AreEqual(verse.SpacelessNormalizedText, "sometestzaherealtest");
+        }
+    }
+}
