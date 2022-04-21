0df4b39b9c4024505d44edffbcdd893ea5537754
Abid Hasan <abidhasan@gmail.com>
2022-04-20 22:34:24 -0700

    Define GQL types

---
+++ Search/SearchWebserver/GraphQL/Query.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotChocolate;
+using Search;
+using SearchWebserver.Services;
+
+namespace SearchWebserver.GraphQL
+{
+    public class Query
+    {
+        private readonly SearchService _searchService;
+
+        public Query(Searcher.SearcherClient grpcClient)
+        {
+            _searchService = new SearchService(grpcClient);
+        }
+
+        public async Task<IEnumerable<SearchType>> GetSearch([GraphQLName("term")] string searchTerm)
+        {
+            var results = await _searchService.DoSearch(searchTerm);
+            return results
+                .Select(res => new SearchType() {
+                    Chapter = res.Chapter, Verse = res.Verse, Score = res.Score, Text = res.Text
+                });
+        }
+    }
+}
---
+++ Search/SearchWebserver/GraphQL/SearchType.cs
@@ -0,0 +1,12 @@
+﻿using Search;
+
+namespace SearchWebserver.GraphQL
+{
+    public class SearchType
+    {
+        public int Chapter { get; set; }
+        public int Verse { get; set; }
+        public double Score { get; set; }
+        public string Text { get; set; }
+    }
+}
