41dfdb950c7281c2864a4f6b48f34739ab08e92e
Abid Hasan <abidhasan@gmail.com>
2022-04-20 22:35:14 -0700

    Create service class for search

---
+++ Search/SearchWebserver/Services/SearchService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Search;
+
+namespace SearchWebserver.Services
+{
+    public class SearchService
+    {
+        private readonly Searcher.SearcherClient _grpcClient;
+
+        public record SearchResult(double Score, int Chapter, int Verse, string Text);
+
+        public SearchService(Searcher.SearcherClient grpcClient)
+        {
+            _grpcClient = grpcClient;
+        }
+
+        public async Task<IEnumerable<SearchResult>> DoSearch(string searchTerm, int count = 10)
+        {
+            var request = new SearchRequest() { SearchTerm = searchTerm };
+            var reply = await _grpcClient.DoSearchAsync(request);
+
+            return reply
+                .SearchResults
+                .OrderByDescending(result => result.Score)
+                .Take(count)
+                .Select(r => new SearchResult(r.Score, r.Chapter, r.Verse, r.Text))
+                .ToArray();
+        }
+    }
+}
